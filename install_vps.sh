23ab2cdb51cdfa1969faf878d015d97be84a15e3
h99178 <34843409+h99178@users.noreply.github.com>
2018-02-10 17:28:31 -0500

    update

--- install_vps.sh
+++ install_vps.sh
@@ -1,20 +1,64 @@
 #!/bin/sh
-cd /
-sudo dd if=/dev/zero of=swapfile bs=1M count=2000
-sudo mkswap swapfile
-sudo swapon swapfile
-echo "/swapfile none swap sw 0 0" >> etc/fstab
-sudo apt-get update -y
-sudo apt-get install make -y
-sudo apt-get install build-essential libtool automake autotools-dev autoconf pkg-config libssl-dev libgmp3-dev libevent-dev bsdmainutils -y
-sudo apt-get install libboost-all-dev -y
-sudo add-apt-repository ppa:bitcoin/bitcoin -y
-sudo apt-get update -y
-sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
-sudo apt-get install libminiupnpc-dev -y
-git clone https://github.com/magnetwork/magnet.git
-cd magnet && \
-chmod +x compile.sh && \
-./compile.sh
-mv /root/magnet/src/magnetd /root/magnet/magnetd
-/root/./magnet/magnetd
+OUTPUT="$(cat /etc/*release)"
+testseq="17.10"
+clear
+if [[ $OUTPUT =~ $testseq ]];
+then
+	echo "=============================================================="
+	echo "|"
+    echo "| No need to compile, just clone Magnetd, I will do it!"
+	echo "|"	
+	echo "=============================================================="
+	sleep 10
+	#installs dependencies
+	sudo apt-get update -y
+	sudo apt-get install make -y
+	sudo apt-get install build-essential libtool automake autotools-dev autoconf pkg-config libssl-dev libgmp3-dev libevent-dev bsdmainutils -y
+	sudo apt-get install libboost-all-dev -y
+	sudo add-apt-repository ppa:bitcoin/bitcoin -y
+	sudo apt-get update -y
+	sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
+	sudo apt-get install libminiupnpc-dev -y
+	#clone magnet
+	mkdir magnet
+	cd magnet
+	wget http://magnetwork.io/Wallets/magnet-qt-LINUX.rar
+	unrar e magnet-qt-LINUX.rar
+	chmod +x magnetd
+	./magnetd
+else
+	echo "=============================================================="
+	echo "|"
+    echo "| This linux version need to compile the Magnetd, I will do it!"
+	echo "|"	
+	echo "=============================================================="
+	sleep 10
+	#create swap of 2GB to have enought memory to compile the Magnet
+	cd /
+	sudo dd if=/dev/zero of=swapfile bs=1M count=2000 
+	sudo mkswap swapfile
+	sudo swapon swapfile
+	echo "/swapfile none swap sw 0 0" >> etc/fstab
+	#installs dependencies
+	sudo apt-get update -y
+	sudo apt-get install make -y
+	sudo apt-get install build-essential libtool automake autotools-dev autoconf pkg-config libssl-dev libgmp3-dev libevent-dev bsdmainutils -y
+	sudo apt-get install libboost-all-dev -y
+	sudo add-apt-repository ppa:bitcoin/bitcoin -y
+	sudo apt-get update -y
+	sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
+	sudo apt-get install libminiupnpc-dev -y
+	#clone repository of Magnet
+	git clone https://github.com/magnetwork/magnet.git
+	#compile Magnetd
+	cd magnet && \
+	chmod +x compile.sh && \
+	./compile.sh
+	#move magnetd file to /root/magnet/ folder
+	mkdir ~/magnet
+	mv /magnet/src/magnetd ~/magnet/magnetd
+	~/magnet/./magnetd
+fi
+
+
+
