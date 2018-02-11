d9f8dc1ef235a47983ec9b2d9d30e1e6ed5c1daa
h99178 <34843409+h99178@users.noreply.github.com>
2018-02-11 13:38:55 -0500

    update

--- install_vps.sh
+++ install_vps.sh
@@ -29,12 +29,18 @@ then
 	echo "=============================================================="
 	echo "|"
     echo "| No need to compile, just clone Magnetd, I will do it!"
-	echo "|"	
+	echo "|"
+	echo "| It will be take 5 minutes :)"	
 	echo "=============================================================="
+	echo "Installation will start in 10 seconds"
 	sleep 10
 	#installs dependencies
+	echo "=============================================================="
+	echo "|"
+    echo "| Installing dependencies"
+	echo "|"	
+	echo "=============================================================="
 	sudo apt-get update -y
-	sudo apt-get install make -y
 	sudo apt-get install build-essential libtool automake autotools-dev autoconf pkg-config libssl-dev libgmp3-dev libevent-dev bsdmainutils -y
 	sudo apt-get install libboost-all-dev -y
 	sudo add-apt-repository ppa:bitcoin/bitcoin -y
@@ -44,21 +50,33 @@ then
 	sudo apt-get install unrar -y
 	sudo apt-get install curl -y
 	#clone magnet
+	echo "=============================================================="
+	echo "|"
+    echo "| Cloning Magnet repository"
+	echo "|"	
+	echo "=============================================================="
 	mkdir magnet
 	cd magnet
 	wget http://magnetwork.io/Wallets/magnet-qt-LINUX.rar
 	unrar e magnet-qt-LINUX.rar
 	chmod +x magnetd
 	./magnetd	
-	printf 'rpcallowip=127.0.0.1\nrpcport=17179\nrpcuser=/RANDOMUSERNAME/\nrpcpassword=/RANDOMPASSWORD/\nserver=1\nlisten=1\ndaemon=1\nport=17177\naddnode=146.148.79.31:17177\naddnode=104.196.202.240:17177\naddnode=35.195.167.40:17177\naddnode=35.199.188.194:17177\naddnode=104.196.155.39:17177\naddnode=35.197.228.109:17177\naddnode=35.198.35.45:17177\naddnode=52.224.232.188:17177\naddnode=150.95.198.182:17177\naddnode=45.76.181.186:17177\naddnode=108.45.164.191:17177\naddnode=45.63.28.187:17177\nexternalip='$EXIP'\nmasternodeaddr='$EXIP':17177\nmasternode=1\nmasternodeprivkey='$PRIVKEY'\n' > /root/.magnet/magnet.conf
+	printf 'rpcallowip=127.0.0.1\nrpcport=17179\nrpcuser=/RANDOMUSERNAME/\nrpcpassword=/RANDOMPASSWORD/\nserver=1\nlisten=1\ndaemon=1\nport=17177\naddnode=146.148.79.31:17177\naddnode=104.196.202.240:17177\naddnode=35.195.167.40:17177\naddnode=35.199.188.194:17177\naddnode=104.196.155.39:17177\naddnode=35.197.228.109:17177\naddnode=35.198.35.45:17177\naddnode=52.224.232.188:17177\naddnode=150.95.198.182:17177\naddnode=45.76.181.186:17177\naddnode=108.45.164.191:17177\naddnode=45.63.28.187:17177\nexternalip='$EXIP'\nmasternodeaddr='$EXIP':17177\nmasternode=1\nmasternodeprivkey='$PRIVKEY'\n' > ~/.magnet/magnet.conf
+	echo "=============================================================="
+	echo "|"
+    echo "| If the message bellow is: 'Magnet server starting' everything is work! You can start your MN on local cold wallet!"
+	echo "|"	
+	echo "=============================================================="
 	./magnetd
 else
 	echo "=============================================================="
 	echo "|"
     echo "| This linux version need to compile the Magnetd, I will do it!"
-	echo "|"	
+	echo "|"
+	echo "| It will be take 30 minutes, have a coffee and relax :)"
 	echo "=============================================================="
-	sleep 10
+	echo "Installation will start in 10 seconds"
+	sleep 10	
 	sudo apt-get install make -y #micro VPS sometimes need to install make
 	#create swap of 2GB to have enought memory to compile the Magnet
 	echo "=============================================================="
@@ -74,7 +92,7 @@ else
 	#installs dependencies
 	echo "=============================================================="
 	echo "|"
-    echo "| Install dependencies"
+    echo "| Installing dependencies"
 	echo "|"	
 	echo "=============================================================="
 	sudo apt-get update -y
@@ -86,7 +104,6 @@ else
 	sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
 	sudo apt-get install libminiupnpc-dev -y
 	#clone repository of Magnet
-	clear
 	echo "=============================================================="
 	echo "|"
     echo "| Clone Magnet repository"
@@ -96,7 +113,7 @@ else
 	#compile Magnetd
 	echo "=============================================================="
 	echo "|"
-    echo "| Clone Magnet repository"
+    echo "| Compiling Magnet repository"
 	echo "|"	
 	echo "=============================================================="
 	cd /
@@ -107,7 +124,12 @@ else
 	mkdir ~/magnet
 	mv /magnet/src/magnetd ~/magnet/magnetd
 	~/magnet/./magnetd
-	printf 'rpcallowip=127.0.0.1\nrpcport=17179\nrpcuser=/RANDOMUSERNAME/\nrpcpassword=/RANDOMPASSWORD/\nserver=1\nlisten=1\ndaemon=1\nport=17177\naddnode=146.148.79.31:17177\naddnode=104.196.202.240:17177\naddnode=35.195.167.40:17177\naddnode=35.199.188.194:17177\naddnode=104.196.155.39:17177\naddnode=35.197.228.109:17177\naddnode=35.198.35.45:17177\naddnode=52.224.232.188:17177\naddnode=150.95.198.182:17177\naddnode=45.76.181.186:17177\naddnode=108.45.164.191:17177\naddnode=45.63.28.187:17177\nexternalip='$EXIP'\nmasternodeaddr='$EXIP':17177\nmasternode=1\nmasternodeprivkey='$PRIVKEY'\n' > /root/.magnet/magnet.conf
+	printf 'rpcallowip=127.0.0.1\nrpcport=17179\nrpcuser=/RANDOMUSERNAME/\nrpcpassword=/RANDOMPASSWORD/\nserver=1\nlisten=1\ndaemon=1\nport=17177\naddnode=146.148.79.31:17177\naddnode=104.196.202.240:17177\naddnode=35.195.167.40:17177\naddnode=35.199.188.194:17177\naddnode=104.196.155.39:17177\naddnode=35.197.228.109:17177\naddnode=35.198.35.45:17177\naddnode=52.224.232.188:17177\naddnode=150.95.198.182:17177\naddnode=45.76.181.186:17177\naddnode=108.45.164.191:17177\naddnode=45.63.28.187:17177\nexternalip='$EXIP'\nmasternodeaddr='$EXIP':17177\nmasternode=1\nmasternodeprivkey='$PRIVKEY'\n' > ~/.magnet/magnet.conf
+	echo "=============================================================="
+	echo "|"
+    echo "| If the message bellow is: 'Magnet server starting' everything is work! You can start your MN on local cold wallet!"
+	echo "|"	
+	echo "=============================================================="
 	~/magnet/./magnetd
 	
 fi
